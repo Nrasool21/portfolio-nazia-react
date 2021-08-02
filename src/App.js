87b6c199831417849e57990df11ade27dc0f68e4
Nazia R <nazia.huma@gmail.com>
2021-08-02 20:36:58 +0400

    styled navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>Portfolio React</div>;
+  return <Navbar />;
 };
 
 export default App;
