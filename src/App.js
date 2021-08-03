454f7b33368bea365d0acdcee740a9cd7cd237b8
Nazia R <nazia.huma@gmail.com>
2021-08-03 15:46:46 +0400

    added css to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />;
+      <Header />;
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info"></div>
+      <h1>Web Developer</h1>
+    </div>
+  );
+};
+
+export default Header;
