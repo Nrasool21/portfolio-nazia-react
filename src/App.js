e8cecb6c9094050f68ac7fa3a5ec209d295b9afe
Nazia R <nazia.huma@gmail.com>
2021-08-05 02:46:34 +0400

    added react particles library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js"
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 
 const App = () => {
   return (
     <>
-      <Navbar />;
-      <Header />;
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
     </>
   );
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Typed from "react-typed"
 
 const Header = () => {
   return (
     <div className="header-wraper">
-      <div className="main-info"></div>
-      <h1>Web Developer</h1>
+      <div className="main-info">
+        <h1>Software Developer</h1>
+        <Typed
+          className="text-typed"
+          strings={[
+            "Front-end Developer",
+            "Back-end Developer",
+            "Javascript",
+            "REACT.js",
+            "Express.js",
+            "MongoBD",
+            "MySQL",
+            "Web Designing",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </div>
     </div>
   );
 };
