6eed6b4c1a8e591e2899d056f090acde116dbf1b
Nazia R <nazia.huma@gmail.com>
2021-08-26 13:38:48 +0400

    refactor navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../my-photo.png"
+import logo from "../../homepageLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,13 +7,12 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
-        <div className="navbar-brand">
-          <img className="logo" src={logo} alt="profile photo" />
-        </div>
-        <div className = "main-info"><h1>Nazia Rasool </h1>
-        <h3>Software Developer</h3>
-        </div>
-      
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={logo} alt="logo.." />
+        </a>
+
+        <h1>Nazia Rasool </h1>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -27,10 +26,15 @@ const Navbar = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav col-md-4 offset-md-8">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">
-                About<span className="sr-only"></span>
+                Home <span className="sr-only"></span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About me
               </a>
             </li>
             <li className="nav-item">
@@ -41,11 +45,6 @@ const Navbar = () => {
             <li className="nav-item">
               <a className="nav-link" href="#">
                 Contact
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Resume
               </a>
             </li>
           </ul>
